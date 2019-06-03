19d7b31b5e78e72dc495313ba3d0ae8320ec8d52
cjihrig <cjihrig@gmail.com>
2019-06-03 09:07:35 -0400

    deps: V8 update postmortem metadata generation script
    Update postmortem metadata constants for V8 7.6 in Node.js.

--- deps/v8/tools/gen-postmortem-metadata.py
+++ deps/v8/tools/gen-postmortem-metadata.py
@@ -165,8 +165,8 @@ consts_misc = [
         'value': 'Map::NumberOfOwnDescriptorsBits::kMask' },
     { 'name': 'bit_field3_number_of_own_descriptors_shift',
         'value': 'Map::NumberOfOwnDescriptorsBits::kShift' },
-    #{ 'name': 'class_Map__instance_descriptors_offset',
-    #    'value': 'Map::kDescriptorsOffset' },
+    { 'name': 'class_Map__instance_descriptors_offset',
+       'value': 'Map::kInstanceDescriptorsOffset' },
 
     { 'name': 'off_fp_context_or_frame_type',
         'value': 'CommonFrameConstants::kContextOrFrameTypeOffset'},
@@ -250,6 +250,7 @@ extras_accessors = [
     'JSArrayBuffer, byte_length, size_t, kByteLengthOffset',
     'JSArrayBufferView, byte_length, size_t, kByteLengthOffset',
     'JSArrayBufferView, byte_offset, size_t, kByteOffsetOffset',
+    'JSTypedArray, external_pointer, uintptr_t, kExternalPointerOffset',
     'JSTypedArray, length, Object, kLengthOffset',
     'Map, instance_size_in_words, char, kInstanceSizeInWordsOffset',
     'Map, inobject_properties_start_or_constructor_function_index, char, kInObjectPropertiesStartOrConstructorFunctionIndexOffset',
